388b72fab96520bfa6428574ce79684261b042d0
Tijs Alleman <tallema2@jh.edu>
2025-10-03 15:01:09 -0400

    Initiate repository (#1)

---
+++ data/README.md
@@ -0,0 +1,37 @@
+# Data readme
+
+Contains an overview of the raw data sources, and the conversion scripts used to convert raw into interim data.
+
+## Raw
+
+### Demography
+
++ `national_state2020.txt`: Contains the 2020 US state names, abbreviation and corresponding two-digit FIPS. Downloaded from https://www.census.gov/library/reference/code-lists/ansi.html
+
+## Interim
+
+### Geography
+
++ `cb_2018_us_state_20m`: US state shape files. Downloaded from: https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html 
+
+### Demography
+
++ `demography.csv`: Population of US states. Columns: 'name_state', 'fips_state', 'name_state', 'population', 'region_name'.
+
+### NHSN-HRD_archive
+
++ `NHSN-HRD_ending-YYYYWW_gathered-YYYY-MM-DD-HH-MM-SS.parquet`: Hospital incidence for COVID-19, Influenza and RSV in every US state and per epiweek, as obtained from the NHSN HRD dataset. Automatically updated and archived weekly through a Github action: `~/.github/workflows/automated_data_collection.yml`. Columns: 'season', 'year', 'MMWR', 'date', 'fips_state', 'name_state', 'influenza admissions', 'covid-19 admissions', 'rsv admissions'.
+
+### Calibration
+
+To be filled out later.
+
+## Conversion
+
+### Demography
+
++ `build-demography.py`: Script used to build the US state-level demography.
+
+### Cases
+
++ `fetch-format_NHSN-HRD-data.py`: Script to collect NHSN HRD data, format it and archive it. Called by through a Github action: `~/.github/workflows/automated_data_collection.yml`.
---
+++ scripts/operational/slackbot.py
@@ -0,0 +1,70 @@
+"""
+This script posts the latest visualisation of the forecast to the Bento lab Slack
+"""
+
+__author__      = "T.W. Alleman"
+__copyright__   = "Copyright (c) 2025 by T.W. Alleman, IDD Group (JHUBSPH) & Bento Lab (Cornell CVM). All Rights Reserved."
+
+import re
+import os
+from datetime import datetime
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Used to find the right forecast
+model_name = 'SIR-1S'
+hyperparameters = 'exclude_None'
+
+# Get the Slack bot token from GH secrets
+CHANNEL_ID = "C07VD6H3Z1B".strip() # Bento Lab "#respiratory-diseases"
+BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("SLACK_BOT_TOKEN not set in environment")
+
+#################################
+## Find latest forecast figure ##
+#################################
+
+def extract_timestamp(fname, pattern):
+    match = pattern.search(fname.name)
+    if match:
+        return datetime.strptime(match.group(0)[-10:], "%Y-%m-%d")
+    return None
+
+from pathlib import Path
+forecast_folder = Path(os.path.join(os.path.dirname(__file__), f'../../data/interim/calibration/forecast/{model_name}/hyperparameters-{hyperparameters}/'))
+pattern = re.compile(r"forecast_reference_date-\d{4}-\d{2}-\d{2}")                                     # regex to capture gathered timestamp
+files_with_time = [(f, extract_timestamp(f, pattern)) for f in forecast_folder.glob("*.png")]          # collect files and their timestamps
+files_with_time = [(f, t) for f, t in files_with_time if t is not None]
+path_to_fig, reference_date = max(files_with_time, key=lambda x: x[1])                                           # get the latest file
+
+#####################
+## Upload to Slack ##
+#####################
+
+# helper function
+def slack_post(path_to_fig, reference_date):
+    """"
+    Post the visualisation in the Bento Lab #respiratory-diseases Slack channel
+    """
+    
+    # Initialize Slack client
+    client = WebClient(token=BOT_TOKEN)
+
+    try:
+        # Upload the image to Slack
+        response = client.files_upload_v2(
+            channel=CHANNEL_ID,
+            file=path_to_fig,
+            filename=f"forecast_reference_date-{reference_date.strftime('%Y-%m-%d')}.png",
+            title=f"CDC FluSight forecast for reference date {reference_date.strftime('%Y-%m-%d')}",
+            initial_comment="This week's 4-week ahead influenza forecast."
+        )
+        print("File uploaded successfully:", response["file"]["permalink"])
+    except SlackApiError as e:
+        print("Error uploading file: {e.response['error']}")
+
+    pass
+
+# post
+slack_post(path_to_fig, reference_date)
